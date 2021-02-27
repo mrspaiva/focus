86580c1f6335b00af40e0eaca754550b3ccbd01a
Natalia <natyap28@hotmail.com>
2021-02-27 20:00:51 -0300

    add ChallengeBox to Homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Profile from '../components/Profile';
 import CompletedChallenges from '../components/CompletedChallenges';
 import styles from '../styles/pages/Home.module.css';
 import Countdown from '../components/Countdown';
+import ChallengeBox from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -21,7 +22,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />  
         </div>
       </section>
     </div>
