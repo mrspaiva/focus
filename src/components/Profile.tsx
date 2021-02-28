bcb00ffc75ed4c4f87d4542dbfe60d5a7bf4265b
Natalia <natyap28@hotmail.com>
2021-02-28 20:49:01 -0300

    use level state

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,18 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../context/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export default function Profile() {
+  const { level } = useContext(ChallengesContext)
+
   return(
     <div className={styles.profileContainer}>
       <img src="https://github.com/mrspaiva.png" alt="profile pic"/>
       <div>
-        <h2>Natalia Rebello Paiva</h2>
+        <h2>Natalia Paiva</h2>
         <p>
           <img src="icons/level.svg" alt="arrow svg"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
