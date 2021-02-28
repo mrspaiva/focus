bfb289485ae2054b0232824eb926c05e146ed8f1
Natalia <natyap28@hotmail.com>
2021-02-27 22:21:46 -0300

    use challengesCompleted function

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../context/ChallengesContext'
 import styles from '../styles/components/CompletedChallenges.module.css'
 
 export default function CompletedChallenges() {
+  const { challengesCompleted } = useContext(ChallengesContext)
+
   return (
     <div className={styles.CompletedChallengesContainer}>
       <p>Desafios Completados</p>
-      <span>00</span>
+      <span>{challengesCompleted}</span>
     </div>
   )
 }
