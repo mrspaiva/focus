df7cb45814b5164af627da303f11967117eda6c3
Natalia <natyap28@hotmail.com>
2021-03-02 20:25:54 -0300

    set the children typescript type

--- src/context/CoundownContext.tsx
+++ src/context/CoundownContext.tsx
@@ -1,12 +1,16 @@
-import { createContext } from "react";
+import { createContext, ReactNode } from "react";
 
 interface CoundownContextData {
 
 }
 
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
 export const CoundownContext = createContext({} as CoundownContextData)
 
-export function CoundownProvider({children}) {
+export function CoundownProvider({children} : ChallengesProviderProps) {
   return (
     <CoundownContext.Provider
     value={{
