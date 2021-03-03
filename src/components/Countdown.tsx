5b51be00ca4be81cb496e412b8e45299a1b0acb0
Natalia <natyap28@hotmail.com>
2021-03-02 22:16:37 -0300

    use coundown context

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,43 +1,19 @@
-import { useContext, useEffect, useState } from 'react'
-import { ChallengesContext } from '../context/ChallengesContext'
+import { useContext } from 'react'
+import { CoundownContext } from '../context/CoundownContext'
 import styles from '../styles/components/Countdown.module.css'
 
-let countdownTimeout: NodeJS.Timeout
-
 export default function Countdown() {
-  const { startNewChallenge } = useContext(ChallengesContext)
-
-  const [time, setTime] = useState(0.1 * 60)
-  const [isActive, setIsActive] = useState(false)
-  const [hasFinished, setHasFinished] = useState(false)
-
-  const minutes = Math.floor(time / 60)
-  const seconds = time % 60
+  const { 
+    minutes, 
+    seconds, 
+    isActive, 
+    hasFinished, 
+    startCountdown, 
+    resetCountdown, 
+  } = useContext(CoundownContext)
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
-
-  function startCountdown() {
-    setIsActive(true)
-  }
-
-  function stopCountdown() {
-    clearTimeout(countdownTimeout)
-    setIsActive(false)
-    setTime(0.1 * 60)
-  }
-
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countdownTimeout = setTimeout(() => {
-        setTime(time - 1)
-      }, 1000)
-    } else if (isActive && time === 0) {
-      setHasFinished(true)
-      setIsActive(false)
-      startNewChallenge()
-    }
-  }, [isActive, time])
 
   return (
     <div>
@@ -68,7 +44,7 @@ export default function Countdown() {
           <button 
             type="button" 
             className={`${styles.CountdownButton} ${styles.CountdownButtonActive}`}
-            onClick={stopCountdown}
+            onClick={resetCountdown}
           >
             Abandonar projeto
             <img className={styles.close} src="icons/close.svg" alt=""/>
