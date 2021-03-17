59647a4526eb1135926cd2811834dcfc39948bdc
Natalia <natyap28@hotmail.com>
2021-03-17 19:55:36 -0300

    store data to cookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
+
 import { ExperienceBar } from "../components/ExperienceBar";
 import Profile from '../components/Profile';
 import CompletedChallenges from '../components/CompletedChallenges';
@@ -6,7 +8,9 @@ import styles from '../styles/pages/Home.module.css';
 import Countdown from '../components/Countdown';
 import ChallengeBox from '../components/ChallengeBox';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
+
   return (
     <div className={styles.container}>
       <Head>
@@ -27,4 +31,17 @@ export default function Home() {
       </section>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async(ctx) => {
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return {
+    props: { 
+      level, 
+      currentExperience, 
+      challengesCompleted 
+    }
+  }
 }
