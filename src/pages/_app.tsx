1f387d61f9a39f1ce1f9ba76cd099407ef39a818
Natalia <natyap28@hotmail.com>
2021-03-18 20:27:37 -0300

    bug fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
-import { ChallengesProvider } from '../context/ChallengesContext'
-import { CoundownProvider } from '../context/CoundownContext'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-      <CoundownProvider>
-        <Component {...pageProps} />
-      </CoundownProvider>
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
